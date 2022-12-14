f82a2b4e64195e1c97bf09180fdda338286805c1
naderhany.fintech <naderhany.fintech@gmail.com>
2022-12-14 16:51:44 +0300

    add dashboard page and structure it

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::prefix('admin')->group( function(){
+    Route::get('dashboard', [AdminController::class, 'dashboard']);
+    Route::match(['get','post'],'login', [AdminController::class, 'login']);
+});
+
